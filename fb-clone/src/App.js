ee6a68117fd0df9392fcd0d6289188fa4d38c240
Myron <josephonyekachi77@gmail.com>
2022-09-26 13:30:35 -0800

    created the header components

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div style={{backgroundColor:'Red'}} className="App">
-      <h1>Home</h1>
+    <div className="app">
+      <Header />
+
+      {/* <h1>Header</h1> */}
+      {/* <h1App body</h1> */}
+        {/* <h1>sidebar</h1> */}
+        {/* <h1>Feeds</h1> */}
+        {/* <h1>Widget</h1> */}
     </div>
   );
 }
---
+++ fb-clone/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+
+        <div className='header__left'></div>
+
+        <div className='header__center'></div>
+
+        <div className='header__right'></div>
+
+    </div>
+  )
+}
+
+export default Header
