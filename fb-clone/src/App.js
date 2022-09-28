66bcb3112f9527a428eace99449c054dd963aa1d
Myron <josephonyekachi77@gmail.com>
2022-09-27 20:37:35 -0800

    updated

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -6,11 +6,10 @@ import Sidebar from './components/Sidebar';
 import Widgets from './components/Widgets';
 
 function App() {
+
   return (
     <div className="app">
       <Header />
-
-
       <div className='app__body'>
 
           <Sidebar />
