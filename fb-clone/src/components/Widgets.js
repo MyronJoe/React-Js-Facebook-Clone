183becfa501f518fccd033920c3a4e3597614918
Myron <josephonyekachi77@gmail.com>
2022-09-30 21:19:05 -0800

    updated

--- fb-clone/src/components/Widgets.js
+++ fb-clone/src/components/Widgets.js
@@ -4,12 +4,11 @@ import './Widgets.css'
 function Widgets() {
   return (
     <div className='widgets'>
-        {/* <iframe title="This is a unique title" id="frame1" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmyron.joe.50&tabs=timeline&width=100%&height=100%&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="100%" height="100%"  scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe> */}
-
-        {/* <iframe title="This is a unique title" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=100%&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="100%" height="500"  scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe> */}
 
         <div className='widget'>
-          <h1>Myron Joe</h1>
+
+          <h1>lorem ipsum (my Facebook widget space - #Myron Joe)</h1>
+
         </div>
     </div>
   )
