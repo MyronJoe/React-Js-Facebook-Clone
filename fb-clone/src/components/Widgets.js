38e2b93bbf6f66ff6432d61e9f541dd284e7e5b5
Myron <josephonyekachi77@gmail.com>
2022-12-06 21:24:09 -0800

    updated

--- fb-clone/src/components/Widgets.js
+++ fb-clone/src/components/Widgets.js
@@ -7,7 +7,7 @@ function Widgets() {
 
         <div className='widget'>
 
-        <a class="twitter-timeline" data-width="400" data-height="500" href="https://twitter.com/KamTech_01?ref_src=twsrc%5Etfw">Tweets by KamTech_01</a> 
+        <iframe title='fram' src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=331&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" className='fram' ></iframe>
 
         </div>
     </div>
